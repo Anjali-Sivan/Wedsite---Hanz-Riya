1ca8caa5010c5847397f3934b200bfd08a39890a
Anjali Sivan <anjali.s@4labsinc.com>
2023-10-19 15:13:41 +0530

    Second_Commit

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2023, 10, 18, 4)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2023, 9, 22, 12)}); // year, month, date, hour
         });
 });		
 
